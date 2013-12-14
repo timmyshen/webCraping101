193f93b6c850354e62658036da1365d8535c73cf
Benqing Shen <benqingshen@gmail.com>
2013-12-13 21:16:43 -0600

    treasury more but not good

--- treasuryScaping.py
+++ treasuryScaping.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 def make_soup(url):
     html = urlopen(url).read()
-    return BeautifulSoup(html, "lxml")
+    return BeautifulSoup(html, "xml")
 
 
 if __name__ == '__main__':
@@ -23,16 +23,18 @@ if __name__ == '__main__':
     link = treasury_url + treasury_resource_center + treasury_data_chart_center + rest_page + data_yield
 
     soup = make_soup(link)
-
-    temp_yield = soup.find("").encode_contents().strip()
+    # print soup.prettify()
+    print soup.table
+    # temp_yield = soup.find("").encode_contents().strip()
     # print type(temp_yield)
     # print temp_yield
+    exit()
 
-
-# you need to find the tag in the source code...
-# the url should be http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2012%20and%20year(NEW_DATE)%20eq%202013
-# <d:NEW_DATE m:type="Edm.DateTime">2013-12-02T00:00:00</d:NEW_DATE>
-# find <d:NEW_DATE m:type="Edm.DateTime"> and get things after that.
+    # you need to find the tag in the source code...
+    # the url should be
+    # http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2012%20and%20year(NEW_DATE)%20eq%202013
+    # <d:NEW_DATE m:type="Edm.DateTime">2013-12-02T00:00:00</d:NEW_DATE>
+    # find <d:NEW_DATE m:type="Edm.DateTime"> and get things after that.
 
     treasuryurl = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2012%20and%20year(NEW_DATE)%20eq%202013'
     soup = make_soup(treasuryurl)
