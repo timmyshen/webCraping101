9b38a1bd535501bb90dc316dc82029afa6999dfd
glowingmeow <chenxy12345@yahoo.com>
2013-12-16 15:55:40 +0800

    I didn't make any change... I just can't run the code properly......

--- treasuryScaping.py
+++ treasuryScaping.py
@@ -21,12 +21,10 @@ if __name__ == '__main__':
     data_yield = "?data=yield"
 
     link = treasury_url + treasury_resource_center + treasury_data_chart_center + rest_page + data_yield
-
     soup = make_soup(link)
     # print soup.prettify()
     # soup_table = soup.find_all('table', "class='t-chart'")
     rows = soup.findAll("tr", {'class': ['oddrow', 'evenrow']})
-
     # for row in rows:
     #     print row
 
@@ -38,7 +36,7 @@ if __name__ == '__main__':
     for one_td in first_row_all_td:
         print one_td.string
 
-    exit()
+    # exit()
 
     # you need to find the tag in the source code...
     # the url should be
@@ -46,14 +44,14 @@ if __name__ == '__main__':
     # <d:NEW_DATE m:type="Edm.DateTime">2013-12-02T00:00:00</d:NEW_DATE>
     # find <d:NEW_DATE m:type="Edm.DateTime"> and get things after that.
 
-    treasuryurl = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2012%20and%20year(NEW_DATE)%20eq%202013'
-    soup = make_soup(treasuryurl)
+    # treasuryurl = 'http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%2012%20and%20year(NEW_DATE)%20eq%202013'
+    # soup = make_soup(treasuryurl)
     # print soup
     # the function soup.find_all(...) may help
-    tagdate = soup.find_all('d:new_date')
-    date = []
-    for tag in tagdate:
-        date.append(tag.text)
-    print date
+    # tagdate = soup.find_all('NEW_DATE')
+    # date = []
+    # for tag in tagdate:
+    #     date.append(tag.text)
+    # print date
     # tag1m = soup.find_all('d:bc_1month')
     # tag3m = soup.find_all('d:BC_3MONTH')
